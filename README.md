2431d889590092959fc1be5f368b43bb0ff71627
Celia Alonso <celiaag94@gmail.com>
2023-03-04 11:07:31 +0000

    wrote the sort part of the readme

--- README.md
+++ README.md
@@ -1,18 +1,40 @@
 # project-3
 
+Planning
+When I was plan
+
+purpose of the website
+
+thought of the ways i wanted the user to be able to interact with the page
+
+and wrote the pseudo code for what I thought would be the next steps to get those functions to work.
+
+
+Building 
+
+I started by creating the html skeleton with a tiny bit of css - that's the part the I was most confortable with.
+
+then I jumped into trying to do the JS side. At first, I didnt know where to start and I begun reading about simple java script and doing bits of courses like freecodecamp or code with mosh. 
+
+At first, I thought I would write the data on the html and then realised that had to be stored in js for everything to work. I started by learing how to add a piece of string stored in JS to the html. Then I wrote the dataObject and managed to render it on the page with a loop.
+
+I started trying to sort the object by title and managed to do it in console.log but didn't now how to make the page itself change.
+
+Before I figured out how to make the page load realised that I coudn't sort by numerical values if the data was sotored in an object so I learnt to store it into an array.
+
+Then I created a reusable function that would draw the movieDataObject and used innerHTML and createElement within it. I reused that funciton when making the buttons sort the movieDataObject by year, runtime, title and rating.
 
 
 
-// Define the HTML structure for the movie list
 
-//link picture to each movie data in js 
+Debugging
 
+I had problems trying sort the movies by year, rating, etc. when the movieData was an object so learnt how to convert the object into and array.
 
-// Function to sort the movies by year
+I also struggled when the title of the movie was the name of the object so I created a new key in the object for title.
 
- 
+I tried to call a constant that was inside a function from outside the fuction and learnt about scope.
 
-// Function to add a new movie to the movieData object
-
+I wanted to have an if statement to that if all the boxes of the form were not filled when submitting a new film it would ask the user to fill them all. But I wrote this if statement on its own without any function and it meant  it was running as soon as the page loaded. I then included it in a fuction.
 
 
--- script.js
+++ script.js
@@ -1,40 +1,40 @@
 // Movie Data
 let movieDataObject = {
-    "The Darjeeling Limited": {
-      plot: "A year after their father's funeral, three brothers travel across India by train in an attempt to bond with each other.",
-      cast: ["Jason Schwartzman", "Owen Wilson", "Adrien Brody"],
-      runtime: 151,
-      rating: 7.2,
-      year: 2007,
-      tag: "darjeeling"
-    },
-    "The Royal Tenenbaums": {
-      plot: "The eccentric members of a dysfunctional family reluctantly gather under the same roof for various reasons",
-      rating: 7.6,
-      year: 2001,
-      cast: ["Gene Hackman", "Gwnyeth Paltrow", "Anjelica Huston"],
-      runtime: 170,
-    },
-    "Fantastic Mr. Fox": {
-      year: 2009,
-      plot: "An urbane fox cannot resist returning to his farm raiding ways and then must help his community survive the farmers' retaliation.",
-      cast: [
-        "George Clooney",
-        "Meryl Streep",
-        "Bill Murray",
-        "Jason Schwartzman",
-      ],
-      runtime: 147,
-      rating: 7.9,
-    },
-    "The Grand Budapest Hotel": {
-      rating: 8.1,
-      runtime: 159,
-      year: 2014,
-      plot: "A writer encounters the owner of an aging high-class hotel, who tells him of his early years serving as a lobby boy in the hotel's glorious years under an exceptional concierge.",
-      cast: ["Ralph Fiennes", "F. Murray Abraham", "Mathieu Amalric"],
-    },
-  };
+  "The Darjeeling Limited": {
+    plot: "A year after their father's funeral, three brothers travel across India by train in an attempt to bond with each other.",
+    cast: ["Jason Schwartzman", "Owen Wilson", "Adrien Brody"],
+    runtime: 151,
+    rating: 7.2,
+    year: 2007,
+    tag: "darjeeling"
+  },
+  "The Royal Tenenbaums": {
+    plot: "The eccentric members of a dysfunctional family reluctantly gather under the same roof for various reasons",
+    rating: 7.6,
+    year: 2001,
+    cast: ["Gene Hackman", "Gwnyeth Paltrow", "Anjelica Huston"],
+    runtime: 170,
+  },
+  "Fantastic Mr. Fox": {
+    year: 2009,
+    plot: "An urbane fox cannot resist returning to his farm raiding ways and then must help his community survive the farmers' retaliation.",
+    cast: [
+      "George Clooney",
+      "Meryl Streep",
+      "Bill Murray",
+      "Jason Schwartzman",
+    ],
+    runtime: 147,
+    rating: 7.9,
+  },
+  "The Grand Budapest Hotel": {
+    rating: 8.1,
+    runtime: 159,
+    year: 2014,
+    plot: "A writer encounters the owner of an aging high-class hotel, who tells him of his early years serving as a lobby boy in the hotel's glorious years under an exceptional concierge.",
+    cast: ["Ralph Fiennes", "F. Murray Abraham", "Mathieu Amalric"],
+  },
+};
 
 // new array for sorting the movie Data
 let movieData = [];
@@ -44,13 +44,13 @@ let sortByAsc = false;
 for(const [key, value] of Object.entries(movieDataObject)){
 
 
-  // join together the key (title of film) and value (film info) to create one new object
-  const newObject = {
-    title: key,
-     ...value
-    };
-    // Add newObject to movieData
-  movieData.push(newObject);
+// join together the key (title of film) and value (film info) to create one new object
+const newObject = {
+  title: key,
+   ...value
+  };
+  // Add newObject to movieData
+movieData.push(newObject);
 }
 
 
@@ -70,137 +70,137 @@ const sortByAscCheckbox = document.querySelector("#sort-by-asc");
 const deleteFirstMovieButton = document.querySelector("#delete-first-movie");
 
 sortByAscCheckbox.addEventListener('change', () => {
-  sortByAsc = sortByAscCheckbox.checked
+sortByAsc = sortByAscCheckbox.checked
 });
 
 sortByYearBtn.addEventListener('click', () => {
-  movieData = movieData.sort((a,b) => 
-  // Inline condition
-    sortByAsc ?  
-      a.year - b.year : 
-      b.year - a.year);
-  drawMovieList();
+movieData = movieData.sort((a,b) => 
+// Inline condition
+  sortByAsc ?  
+    a.year - b.year : 
+    b.year - a.year);
+drawMovieList();
 });
 
 sortByRatingBtn.addEventListener('click', ()=> {
-    movieData = movieData.sort((a,b) =>
-    sortByAsc ?
-      a.rating - b.rating :
-      b.rating - a.rating);
-      drawMovieList();
-  });
-
-sortByRuntimeBtn.addEventListener('click', ()=> {
   movieData = movieData.sort((a,b) =>
   sortByAsc ?
-    a.runtime - b.runtime :
-    b.runtime - a.runtime);
-  drawMovieList();
+    a.rating - b.rating :
+    b.rating - a.rating);
+    drawMovieList();
 });
 
-  // Example of above inline condition but not inline
-  // if(sortByAsc){
-  //   return a.year - b.year;
-  // }
-  // else{
-  //   return b.year - a.year;
-  // }
+sortByRuntimeBtn.addEventListener('click', ()=> {
+movieData = movieData.sort((a,b) =>
+sortByAsc ?
+  a.runtime - b.runtime :
+  b.runtime - a.runtime);
+drawMovieList();
+});
+
+// Example of above inline condition but not inline
+// if(sortByAsc){
+//   return a.year - b.year;
+// }
+// else{
+//   return b.year - a.year;
+// }
 
 deleteFirstMovieButton.addEventListener('click', () => {
-  if(movieData.length < 1)
-    return;
+if(movieData.length < 1)
+  return;
 
-  const removedElements = movieData.splice(0, 1);
-  console.log(movieData, removedElements);
-  drawMovieList();
+const removedElements = movieData.splice(0, 1);
+console.log(movieData, removedElements);
+drawMovieList();
 });
 
 function clearMovieList(){
-  // Clear the moviesContainers children
-  moviesContainer.innerHTML = '';
+// Clear the moviesContainers children
+moviesContainer.innerHTML = '';
 }
 // Create new re-usable function that will CLEAR the movieContainer, and REDRAW the current movieData into the container
 function drawMovieList(){
-  clearMovieList();
-  for (const movie of movieData) {  
-    // Create a new HTML element for each movie
-    const movieElement = document.createElement('div');
-  // Add the movie title, year, plot, cast, runtime, and rating to the element
-  movieElement.innerHTML = `
-    <h2>${movie.title}</h2>
-    <p><strong>Year:</strong> ${movie.year}</p>
-    <p><strong>Plot:</strong> ${movie.plot}</p>
-    <p><strong>Cast:</strong> ${movie.cast.join(', ')}</p>
-    <p><strong>Runtime:</strong> ${movie.runtime} minutes</p>
-    <p><strong>Rating:</strong> ${movie.rating}</p>
-  `;
+clearMovieList();
+for (const movie of movieData) {  
+  // Create a new HTML element for each movie
+  const movieElement = document.createElement('div');
+// Add the movie title, year, plot, cast, runtime, and rating to the element
+movieElement.innerHTML = `
+  <h2>${movie.title}</h2>
+  <p><strong>Year:</strong> ${movie.year}</p>
+  <p><strong>Plot:</strong> ${movie.plot}</p>
+  <p><strong>Cast:</strong> ${movie.cast.join(', ')}</p>
+  <p><strong>Runtime:</strong> ${movie.runtime} minutes</p>
+  <p><strong>Rating:</strong> ${movie.rating}</p>
+`;
 
-    // Add the movie element to the movies container
-    moviesContainer.appendChild(movieElement);
-  }
+  // Add the movie element to the movies container
+  moviesContainer.appendChild(movieElement);
+}
 }
 drawMovieList();
 
 //  Form for user to add new movie
 
-  // Select the form
+// Select the form
 
 const form = document.getElementById("entry-form");
 
 form.addEventListener("submit", (e) => {
-  let titleInput = document.getElementById("in-title");
-  let yearInput = document.getElementById("in-year");
-  let plotInput = document.getElementById("in-plot");
-  let ratingInput = document.getElementById("in-rating");
-  let runtimeInput = document.getElementById("in-runtime");
-  // cast
+let titleInput = document.getElementById("in-title");
+let yearInput = document.getElementById("in-year");
+let plotInput = document.getElementById("in-plot");
+let ratingInput = document.getElementById("in-rating");
+let runtimeInput = document.getElementById("in-runtime");
+// cast
 
-  if (titleInput.value == "" || yearInput.value == "" || plotInput.value == "" || ratingInput.value == "" || runtimeInput.value == "") {
-    alert("Ensure you input a value in all fields!");
-  } else {
-    // perform operation with form input
-    alert("Your new film has been successfully submitted!");
-    console.log(
-      `This form has a tile of ${titleInput.value} and a rating of ${ratingInput.value}`
-    );
+if (titleInput.value == "" || yearInput.value == "" || plotInput.value == "" || ratingInput.value == "" || runtimeInput.value == "") {
+  alert("Ensure you input a value in all fields!");
+} else {
+  // perform operation with form input
+  alert("Your new film has been successfully submitted!");
+  console.log(
+    `This form has a tile of ${titleInput.value} and a rating of ${ratingInput.value}`
+  );
 
-    const newTitle = titleInput.value; 
-    const newPlot = plotInput.value;
-    const newYear = yearInput.value;
-    const newRating = ratingInput.value;
-    const newRuntime = runtimeInput.value;
+  const newTitle = titleInput.value; 
+  const newPlot = plotInput.value;
+  const newYear = yearInput.value;
+  const newRating = ratingInput.value;
+  const newRuntime = runtimeInput.value;
 
-  function drawNewMovieList(){
-    clearNewMovieList();
-      for (const movie of form) {  
-        // Create a new HTML element for each movie
-        const movieNewElement = document.createElement('div');
-    movieNewElement.innerHTML = 
-    `
-      <h2>${newTitle}</h2>
-      <p><strong>Year:</strong> ${newYear}</p>
-      <p><strong>Plot:</strong> ${newPlot}</p>
-      // <p><strong>Cast:</strong> ${movie.cast.join(', ')}</p>
-      <p><strong>Runtime:</strong> ${newRuntime} minutes</p>
-      <p><strong>Rating:</strong> ${newRating}</p>
-    `;
+function drawNewMovieList(){
+  clearNewMovieList();
+    for (const movie of form) {  
+      // Create a new HTML element for each movie
+      const movieNewElement = document.createElement('div');
+  movieNewElement.innerHTML = 
+  `
+    <h2>${newTitle}</h2>
+    <p><strong>Year:</strong> ${newYear}</p>
+    <p><strong>Plot:</strong> ${newPlot}</p>
+    // <p><strong>Cast:</strong> ${movie.cast.join(', ')}</p>
+    <p><strong>Runtime:</strong> ${newRuntime} minutes</p>
+    <p><strong>Rating:</strong> ${newRating}</p>
+  `;
 
-    moviesContainer.appendChild(movieNewElement);
-      }
+  moviesContainer.appendChild(movieNewElement);
     }
-    drawNewMovieList();
-    
-    titleInput.value = "";
-    plotInput.value = "";
-    runtimeInput.value = "";
-    ratingInput.value = "";
-    yearInput.value = "";
-
-    movieData.push(movieNewElement);
   }
-    
+  drawNewMovieList();
+  
+  titleInput.value = "";
+  plotInput.value = "";
+  runtimeInput.value = "";
+  ratingInput.value = "";
+  yearInput.value = "";
 
-    // handle submit
+  movieData.push(movieNewElement);
+}
+  
+
+  // handle submit
 });
 
 
@@ -208,11 +208,11 @@ form.addEventListener("submit", (e) => {
 
 // if else was not inside a fuction so was just running
 
-  // 	function addNewMovie() {
-  //     document.getElementById("form").innerHTML = ;
-  //   }
-  // });
-  
+// 	function addNewMovie() {
+//     document.getElementById("form").innerHTML = ;
+//   }
+// });
+
 
 // form.addEventListener('submit', function () {
 // 	function addNewMovie() {
@@ -236,14 +236,14 @@ form.addEventListener("submit", (e) => {
 
 
 
-  // function sortMoviesByYear() {
-  //   const sortedMovieTitles = Object.keys(movieData).sort((a, b) => {
-  //     return movieData[a].year - movieData[b].year;
-  //   });
+// function sortMoviesByYear() {
+//   const sortedMovieTitles = Object.keys(movieData).sort((a, b) => {
+//     return movieData[a].year - movieData[b].year;
+//   });
 
 
-  // chat gpt - says I'm missing last stepts
-   // Remove existing movie elements from the movies container
+// chat gpt - says I'm missing last stepts
+ // Remove existing movie elements from the movies container
 //    moviesContainer.innerHTML = '';
 
 //    // Add new movie elements in the sorted order
@@ -262,59 +262,59 @@ form.addEventListener("submit", (e) => {
 //    });
 //  }
 
-  //   sortByYear.addEventListener("click", sortMoviesByYear);
+//   sortByYear.addEventListener("click", sortMoviesByYear);
 
 
 // 1 way
-    // let locationHTML = document.querySelector("#movies-container");
-    // locationHTML.innerHTML = movieData["The Grand Budapest Hotel"].plot;
-
-    
-
-    // button.addEventListener("click", function() {
-    
-    // });
-    // });
-
-
-
-
-
-
-  // const movieDataArray = Object.entries(movieData);
-
-   // movieDataArray.forEach(movie => { 
-   
-  //   movies.insertAdjacentHTML("beforeend", 
-  //   `
-  //   <div> 
-  //   <h1>${movie[0]}</h1>
-  //   <h2>${movie[1].plot}</h2>
-  //   </div>
-  //   `)
-  // })
+  // let locationHTML = document.querySelector("#movies-container");
+  // locationHTML.innerHTML = movieData["The Grand Budapest Hotel"].plot;
 
   
 
+  // button.addEventListener("click", function() {
+  
+  // });
+  // });
 
-  // showProperties(movie);
-  // function showProperties(obj) {
-  // for (let key in obj)}
 
-  // for (const [key, value] of Object.entries(movieData))
 
-  // console.log(key);
 
+
+
+// const movieDataArray = Object.entries(movieData);
+
+ // movieDataArray.forEach(movie => { 
  
+//   movies.insertAdjacentHTML("beforeend", 
+//   `
+//   <div> 
+//   <h1>${movie[0]}</h1>
+//   <h2>${movie[1].plot}</h2>
+//   </div>
+//   `)
+// })
 
 
-  // foreach //queryslector/  insertadjacementHTML
+
+
+// showProperties(movie);
+// function showProperties(obj) {
+// for (let key in obj)}
+
+// for (const [key, value] of Object.entries(movieData))
+
+// console.log(key);
+
+
+
+
+// foreach //queryslector/  insertadjacementHTML
 
 //  conditional statements
 
 
- 
-  // Iterate over each movie in the movieData object
+
+// Iterate over each movie in the movieData object
 // for (const title in movieData) {
 //   // Get a reference to the movie object
 //   const movie = movieData[title];
